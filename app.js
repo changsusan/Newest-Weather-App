1e922536564e81c1f510fee64a82d2c434175374
changsusan <90660832+changsusan@users.noreply.github.com>
2021-11-29 20:17:33 -0500

    Showing current temperature

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("Hello");
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.main.humidity);
+  console.log(response.data.wind.speed);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+let apikey = "8bfbf28022488dee7340fe98270ce789";
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apikey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
