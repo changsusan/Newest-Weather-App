f2b8c0b8a43ae9595647ae99fd6debb12f998ddc
changsusan <90660832+changsusan@users.noreply.github.com>
2021-12-08 00:01:50 -0500

    edited typo error in Javascript

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["SUn", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 function displayForecast(response) {
